d9e74fdde3f73e63f881ac543fa7d52d1a185842
Anto Geoby Rayan <anto.geobyrayan@tesco.com>
2020-08-07 09:42:40 +0530

    Integrated the changes with PromotionEngine

--- src/PromotionEngine.cs
+++ src/PromotionEngine.cs
@@ -1,63 +1,35 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Promotion
 {
     public class PromotionEngine
     {
-        private readonly int totalUnitA;
-        private readonly int totalUnitB;
-        private readonly int totalUnitC;
-        private readonly int totalUnitD;
+        private readonly OrderDetails myOrderDetails;
 
-        private readonly int myProductAValue = 50;
-
-        private readonly int myProductBValue = 30;
-
-        private readonly int myProductCValue = 20;
-
-        private readonly int myProductDValue = 15;
-
-        public PromotionEngine(int totalUnitA, int totalUnitB, int totalUnitC, int totalUnitD)
+        public PromotionEngine(OrderDetails orderDetails)
         {
-            this.totalUnitA = totalUnitA;
-            this.totalUnitB = totalUnitB;
-            this.totalUnitC = totalUnitC;
-            this.totalUnitD = totalUnitD;
+            myOrderDetails = orderDetails;
         }
 
-        public double GetDiscountedAmount()
+        public int ApplyPromotion()
         {
+            var promotionFactory = new PromotionFactory();
+            var promotionsApplicable = promotionFactory.GetPromotions();
             var totalValueOfProducts = 0;
 
-            //Calculation of product A
-            if (this.totalUnitA > 0)
+            foreach(var promotion in promotionsApplicable)
             {
-                var promotionalValue = (int)Math.Floor((double)(this.totalUnitA / 3));
-                totalValueOfProducts += (promotionalValue * 130) + ((this.totalUnitA % 3) * myProductAValue);
-            }
-
-            //Calculation of Product B
-            if (this.totalUnitB > 0)
-            {
-                var promotionalValue = (int)Math.Floor((double)(this.totalUnitB / 2));
-                totalValueOfProducts += (promotionalValue * 45) + ((this.totalUnitB % 2) * myProductBValue);
-            }
-
-            //Calculation of Product C and D
-            var leastValuedProduct = Math.Min(this.totalUnitC, this.totalUnitD);
-            var maxValueProduct = Math.Max(this.totalUnitC, this.totalUnitD);
-
-            if(this.totalUnitC > this.totalUnitD) {
-                totalValueOfProducts += ((leastValuedProduct * 30) + ((maxValueProduct-leastValuedProduct) * myProductCValue));
-            }
-            else if(this.totalUnitD > this.totalUnitC){
-                totalValueOfProducts += ((leastValuedProduct * 30) + ((maxValueProduct-leastValuedProduct) * myProductDValue));
-            }
-            else {
-                totalValueOfProducts += (leastValuedProduct * 30);
+                var products = GetProductsLinkedWithThePromotion(promotion);
+                totalValueOfProducts += promotion.Apply(products);
             }
 
             return totalValueOfProducts;
+        }
+
+        private IList<Product.IProduct> GetProductsLinkedWithThePromotion(IPromotion promotion)
+        {
+            return myOrderDetails.Products.Where(product => promotion.PromotionLinkedWithPlan.Contains(product.ProductName)).ToList();
         }
     }
 }
